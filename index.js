aebaa45ed000f4915f7ae1fcf96d94de6df3e5ee
Rahul Kumar <rahul4michan@gmail.com>
2020-10-28 19:50:06 +0530

    files from sftp downloaded

---
+++ index.js
@@ -0,0 +1,41 @@
+const Client = require('ssh2').Client;
+const fs = require('fs');
+const conn = new Client();    
+const sshOpt = {
+    host: 'test.rebex.net',
+    username: 'demo',
+    password: 'password'
+};    
+const remoteFile = '/pub/example/readme.txt';
+const localdir = 'temp';
+const localZipFileName = 'sample.zip';
+const localFile = localdir + '/' + localZipFileName;
+try{
+    if (!fs.existsSync(localdir)){
+        fs.mkdirSync(localdir);
+    }
+    else{
+        fs.rmSync(localFile);
+    }
+    
+    conn.on('ready', () => {
+      conn.sftp((err, sftp) => {
+        if (err) throw err;
+            console.log('Downloading ' + remoteFile);
+            sftp.fastGet(remoteFile, localFile, (err) => {
+              if (err) throw err;
+              console.log('Downloaded to ' + localFile);
+              conn.end();
+              
+            });
+        });
+    });
+    conn.on('error', (err) => {
+        console.error('SSH connection stream problem');
+        throw err;
+    });
+    conn.connect(sshOpt);
+}
+catch(ex){
+    console.log(ex);
+}
