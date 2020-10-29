4fc8ef741434568d0ba65606df0b63c0354a6431
Rahul Kumar <58619937+app-rahul@users.noreply.github.com>
2020-10-29 19:28:05 +0530

    Fill the required Details before using

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    sshOpt : {
+        host: '',
+        username: '',
+        password: ''
+    },
+    remoteZipFilePath : '',
+    zipFilePassword : '',
+    localdir : 'temp',
+    localZipFileName : 'sample.zip'
+}
